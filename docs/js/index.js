e416cccdccded16faa5a1d1aaae1ed3b6b086434
The Blob SCP <81673375+theblobscp@users.noreply.github.com>
2021-07-03 23:02:52 -0400

    Create index.js

---
+++ docs/js/index.js
@@ -0,0 +1,4 @@
+// get major elements ig (I need something to put here again)
+const html = document.getElementsByTagName("html");
+const head = document.getElementsByTagName("head");
+const body = document.getElementsByTagName("body");
